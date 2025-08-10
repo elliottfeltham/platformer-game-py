0ab7d28dbf3d91da98a0b819298f2bbc6cc3f950
elliottfeltham <elliottfeltham98@gmail.com>
2025-08-10 22:46:53 +0100

    Add boilerplate and assets

---
+++ game.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption("Platform Game")
+
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
