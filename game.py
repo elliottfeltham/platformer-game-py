ad45187ad74d21e509d84f3f63acea0eb6257372
elliottfeltham <elliottfeltham98@gmail.com>
2025-08-11 11:22:46 +0100

    Add basic physics and tilemap

--- game.py
+++ game.py
@@ -1,31 +1,44 @@
 import pygame
 import sys
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self):
         pygame.init()
 
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.surface.Surface((320, 240))
         pygame.display.set_caption("Platform Game")
 
         self.clock = pygame.time.Clock()
 
-        self.movement = [False, False] # Booleans chacking if a button is held down
+        self.movement = [False, False] # Booleans checking if LEFT or RIGHT button is held down
 
         self.assets = {
-            "player": load_image("entities/player.png")
+            "player": load_image("entities/player.png"),
+            "decor": load_images("tiles/decor"),
+            "grass": load_images("tiles/grass"),
+            "large_decor": load_images("tiles/large_decor"),
+            "stone": load_images("tiles/stone"),
         }
 
         self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
 
+        self.tilemap = Tilemap(self, tile_size=16)
+
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
 
-            self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.tilemap.render(self.display)
+
+            x_axis_movement = self.movement[1] - self.movement[0] # Equates to either 1, -1 or 0 based on keyboard input
+            self.player.update(self.tilemap, (x_axis_movement, 0)) 
+            self.player.render(self.display)
+
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -43,7 +56,7 @@ class Game:
                         self.movement[1] = False
 
 
-
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()))
             pygame.display.update()
             self.clock.tick(60)
 
--- scripts/entities.py
+++ scripts/entities.py
@@ -7,12 +7,43 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.velocity = [0, 0]
+
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
         
-    def update(self, movement=(0, 0)):
+    def update(self, tilemap, movement=(0, 0)):
         # Control for X and Y axis seperatley
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        # X Axis
         self.pos[0] += frame_movement[0]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                self.pos[0] = entity_rect.x
+
+
+        # Y Axis
         self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                self.pos[1] = entity_rect.y
+
+        if self.pos[1] == 800:
+            self.pos[1] = -100
+        
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
     def render(self, surf):
         surf.blit(self.game.assets["player"], self.pos)
